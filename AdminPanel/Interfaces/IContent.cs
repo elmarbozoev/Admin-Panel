f2acefae0357bb75d570818eb843dcbd1c31952f
luvkarina <116797390+papajok@users.noreply.github.com>
2022-11-12 14:44:08 +0600

    interfaces

---
+++ AdminPanel/Interfaces/IContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Interfaces
+{
+    public interface IContent
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }    
+        string Media { get; set; }
+        Task<IActionResult> Create();
+        Task<IActionResult> Edit();
+        Task<IActionResult> Delete();
+
+
+    }
+}
