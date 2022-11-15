b4f64364cdb12b21549f4d7b1846469f4c9cd4b5
elmarbozoev <116433754+elmarbozoev@users.noreply.github.com>
2022-11-15 15:39:11 +0600

    stashed changes in icontent

--- AdminPanel/Interfaces/IContent.cs
+++ AdminPanel/Interfaces/IContent.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPanel.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AdminPanel.Interfaces
 {
     public interface IContent 
     {
         int Id { get; set; }
+<<<<<<< Updated upstream
         string Name { get; set; }
         string Description { get; set; }    
         string Media { get; set; }
@@ -13,5 +15,10 @@ namespace AdminPanel.Interfaces
         Task<IActionResult> Delete();
 
 
+=======
+        string? Name { get; set; }
+        string? Description { get; set; }    
+        ICollection<Picture>? Pictures { get; set; }
+>>>>>>> Stashed changes
     }
 }
