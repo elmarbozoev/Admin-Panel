5f75ce273012eacd2f29a7e69d79f5dcfbaa718d
elmarbozoev <116433754+elmarbozoev@users.noreply.github.com>
2022-11-15 15:43:57 +0600

    fix conflict attempt 1

--- AdminPanel/Interfaces/IContent.cs
+++ AdminPanel/Interfaces/IContent.cs
@@ -6,19 +6,11 @@ namespace AdminPanel.Interfaces
     public interface IContent 
     {
         int Id { get; set; }
-<<<<<<< Updated upstream
         string Name { get; set; }
         string Description { get; set; }    
         string Media { get; set; }
         Task<IActionResult> Create();
         Task<IActionResult> Edit();
         Task<IActionResult> Delete();
-
-
-=======
-        string? Name { get; set; }
-        string? Description { get; set; }    
-        ICollection<Picture>? Pictures { get; set; }
->>>>>>> Stashed changes
     }
 }
