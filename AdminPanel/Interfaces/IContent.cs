bda41baa6793d49cfbfa925bb141f84b9e6019e3
elmarbozoev <116433754+elmarbozoev@users.noreply.github.com>
2022-11-15 15:48:20 +0600

    fix conflict final attempt

--- AdminPanel/Interfaces/IContent.cs
+++ AdminPanel/Interfaces/IContent.cs
@@ -8,5 +8,6 @@ namespace AdminPanel.Interfaces
         int Id { get; set; }
         string? Name { get; set; }
         string? Description { get; set; }    
+        ICollection<Picture>? Pictures { get; set; }
     }
 }
