f9bcc9d3fb655a414b6f8a09ae29d931fefcc174
elmarbozoev <116433754+elmarbozoev@users.noreply.github.com>
2022-11-15 15:46:46 +0600

    fix conflict attempt 2

--- AdminPanel/Interfaces/IContent.cs
+++ AdminPanel/Interfaces/IContent.cs
@@ -8,9 +8,5 @@ namespace AdminPanel.Interfaces
         int Id { get; set; }
         string Name { get; set; }
         string Description { get; set; }    
-        string Media { get; set; }
-        Task<IActionResult> Create();
-        Task<IActionResult> Edit();
-        Task<IActionResult> Delete();
     }
 }
