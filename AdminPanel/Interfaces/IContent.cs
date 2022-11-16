0ae7f508773a780eb827dd0f8f70a4a34fa57d51
Semaeva <wunata95@gmail.com>
2022-11-16 17:23:05 +0600

    code-review
    оставила комментарии по News контролллеру

--- AdminPanel/Interfaces/IContent.cs
+++ AdminPanel/Interfaces/IContent.cs
@@ -2,13 +2,19 @@
 
 namespace AdminPanel.Interfaces
 {
-    public interface IContent 
+    public interface IContent  
     {
+        //рекомендация
+        // так как в "достижениях" тоже будет фото/видео, то проще класс Pictures переименовать в AffFiles/Media или еще как-нибудь
+        //и так как CRUD везде одинаковые,нужно создать еще один интерфейс с операоцяими CRUD, включая CreateFile.
+        //Каждый контролллер имплементировать от интерфейса
         int Id { get; set; }
         string Name { get; set; }
         string Description { get; set; }    
         string Media { get; set; }
 
+
+        //это зачем тут? новый интерфейс нужен. Выше описала
         Task<IActionResult> Create();
         Task<IActionResult> Edit();
         Task<IActionResult> Delete();
